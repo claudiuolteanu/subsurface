a0cafadaee338002bb0ed131bd8300336683ba6c
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2015-06-21 20:52:08 +0300

    Declare a serial_t structure similar with the one from native serial implementation.

--- qtserialbluetooth.h
+++ qtserialbluetooth.h
@@ -3,6 +3,19 @@
 
 #include <libdivecomputer/custom_serial.h>
 
+#include <QtBluetooth/QBluetoothAddress>
+#include <QtBluetooth/QBluetoothSocket>
+
+struct serial_t {
+	/* Library context. */
+	dc_context_t *context;
+	/*
+	 * The file descriptor corresponding to the serial port.
+	 */
+	QBluetoothSocket *socket;
+	long timeout;
+};
+
 dc_status_t dc_serial_qt_open(dc_serial_t **out, dc_context_t *context, const char *devaddr);
 
 #endif // QTSERIALBLUETOOTH_H
